9fdd742edce8300f71a8de50947276da53738406
rc19002 <rc19002@melville-high.school.nz>
2023-08-07 09:39:19 +1200

    Still working on a way to change the question label through a click of the button multiple times.

--- main.py
+++ main.py
@@ -20,13 +20,39 @@ def quiz():
     menu_frame.destroy()  # destroy Menu
     question_label = Label(root, text="Question")
     question_label.grid(row=0, column=0, padx=5, pady=5)
-    answer1_button = ttk.Button(root, text="A.< >")
+
+    def change_text():  # changing the question Label
+        global button_counter
+        if button_counter == 0:
+            question_label.config(text="Question 2")
+        elif button_counter == 1:
+            question_label.config(text="Question 3")
+        elif button_counter == 2:
+            question_label.config(text="Question 4")
+        elif button_counter == 3:
+            question_label.config(text="Question 5")
+        elif button_counter == 4:
+            question_label.config(text="Question 6")
+        elif button_counter == 5:
+            question_label.config(text="Question 7")
+        elif button_counter == 6:
+            question_label.config(text="Question 8")
+        elif button_counter == 7:
+            question_label.config(text="Question 9")
+        else:
+            question_label.config(text="Question 10")
+        if button_counter != 9:
+            button_counter += 1
+        else:
+            button_counter = 0
+    answer1_button = ttk.Button(root, text="A.< >", command=change_text)
+    button_counter = 0
     answer1_button.grid(row=1, column=0, padx=5, pady=5)
-    answer2_button = ttk.Button(root, text="B.< >")
+    answer2_button = ttk.Button(root, text="B.< >", command=change_text)
     answer2_button.grid(row=2, column=0, padx=5, pady=5)
-    answer3_button = ttk.Button(root, text="C.< >")
+    answer3_button = ttk.Button(root, text="C.< >", command=change_text)
     answer3_button.grid(row=1, column=1, padx=5, pady=5)
-    answer4_button = ttk.Button(root, text="D.< >")
+    answer4_button = ttk.Button(root, text="D.< >", command=change_text)
     answer4_button.grid(row=2, column=1, padx=5, pady=5)
     confirm_button = ttk.Button(root, text="Confirm")
     confirm_button.grid(row=3, column=0, padx=5, pady=5)
