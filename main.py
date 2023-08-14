c450e1c594b4d98fd9df6c29f810d285c14bd4d9
rc19002 <rc19002@melville-high.school.nz>
2023-08-15 10:39:48 +1200

    Point system is still work in progress including almost most of the codes.

--- main.py
+++ main.py
@@ -18,26 +18,26 @@ def menu():
 # main quiz program
 def quiz():
     menu_frame.destroy()  # destroy Menu
-    question_label = Label(root, text="A story of a rubber boy.")
+    question_label = Label(root, text="A story of a rubber boy")
     question_label.grid(row=0, column=0, padx=5, pady=5)
     point_label = Label(root, text="Point: 0")
     point_label.grid(row=3, column=1, padx=5, pady=5)
 
     def change_text():  # changing the question Label
         if Button.counter == 0:
-            question_label.config(text="a scientist is the new stone world.")
+            question_label.config(text="a scientist is the new stone world")
             answer1_button.config(text="Dr. Stone")
             answer2_button.config(text="New Game")
             answer3_button.config(text="Dr. Rock")
             answer4_button.config(text="Stone World")
         elif Button.counter == 1:
-            question_label.config(text="Teen eats a cursed item and turn into a King.")
+            question_label.config(text="Teen eats a cursed item and turn into a King")
             answer1_button.config(text="Demon Slayer")
             answer2_button.config(text="Dark Karate")
             answer3_button.config(text="Jujutsu Kaisen")
             answer4_button.config(text="Code Geass")
         elif Button.counter == 2:
-            question_label.config(text="Family bound by the gravity of fate.")
+            question_label.config(text="Family bound by the gravity of fate")
             answer1_button.config(text="Hunter Hunter")
             answer2_button.config(text="Dragon ball Z")
             answer3_button.config(text="Inazuma 11")
@@ -57,7 +57,7 @@ def quiz():
         elif Button.counter == 5:
             question_label.config(text="Man-eating Giants terrorize cities")
             answer1_button.config(text="Question 2")
-            answer2_button.config(text="Question 2")
+            answer2_button.config(text="Attack On Titan")
             answer3_button.config(text="Question 2")
             answer4_button.config(text="Question 2")
         elif Button.counter == 6:
@@ -83,6 +83,12 @@ def quiz():
         else:
             Button.counter = 0
 
+    def point_system():
+        point = 0
+        Button.counter = 0
+        if Button.counter in range 0
+        point_label.config(text="Point:{}". format(point))
+
     button = IntVar()
     answer1_button = ttk.Radiobutton(root, text="A. MHA", variable=button, value=1)
     answer1_button.grid(row=1, column=0, padx=5, pady=5)
@@ -92,7 +98,7 @@ def quiz():
     answer3_button.grid(row=1, column=1, padx=5, pady=5)
     answer4_button = ttk.Radiobutton(root, text="D. God of High school", variable=button, value=4)
     answer4_button.grid(row=2, column=1, padx=5, pady=5)
-    confirm_button = ttk.Button(root, text="Confirm", command=change_text)
+    confirm_button = ttk.Button(root, text="Confirm", command=lambda: [(change_text(), point_system())])
     Button.counter = 0
     confirm_button.grid(row=3, column=0, padx=5, pady=5)
 
