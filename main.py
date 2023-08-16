a3a5c091746889e28bfb0a98810b5ab15163708e
rc19002 <rc19002@melville-high.school.nz>
2023-08-16 12:54:43 +1200

    Still doing the same thing but making it a little less messy

--- main.py
+++ main.py
@@ -24,70 +24,20 @@ def quiz():
     point_label.grid(row=3, column=1, padx=5, pady=5)
 
     def change_text():  # changing the question Label
-        if Button.counter == 0:
-            question_label.config(text="a scientist is the new stone world")
-            answer1_button.config(text="Dr. Stone")
-            answer2_button.config(text="New Game")
-            answer3_button.config(text="Dr. Rock")
-            answer4_button.config(text="Stone World")
-        elif Button.counter == 1:
-            question_label.config(text="Teen eats a cursed item and turn into a King")
-            answer1_button.config(text="Demon Slayer")
-            answer2_button.config(text="Dark Karate")
-            answer3_button.config(text="Jujutsu Kaisen")
-            answer4_button.config(text="Code Geass")
-        elif Button.counter == 2:
-            question_label.config(text="Family bound by the gravity of fate")
-            answer1_button.config(text="Hunter Hunter")
-            answer2_button.config(text="Dragon ball Z")
-            answer3_button.config(text="Inazuma 11")
-            answer4_button.config(text="JoJo's Bizarre Adventure")
-        elif Button.counter == 3:
-            question_label.config(text="Lovestruck man proves himself through basketball")
-            answer1_button.config(text="Slam Dunk")
-            answer2_button.config(text="Kuroko no Basket")
-            answer3_button.config(text="Fruit Basket")
-            answer4_button.config(text="Dribble")
-        elif Button.counter == 4:
-            question_label.config(text="Humankind in a brink of destruction by gods")
-            answer1_button.config(text="God Of High school")
-            answer2_button.config(text="Saint Young Man")
-            answer3_button.config(text="Record Of Ragnarok")
-            answer4_button.config(text="Overlord")
-        elif Button.counter == 5:
-            question_label.config(text="Man-eating Giants terrorize cities")
-            answer1_button.config(text="Question 2")
-            answer2_button.config(text="Attack On Titan")
-            answer3_button.config(text="Question 2")
-            answer4_button.config(text="Question 2")
-        elif Button.counter == 6:
-            question_label.config(text="Question 8")
-            answer1_button.config(text="Question 2")
-            answer2_button.config(text="Question 2")
-            answer3_button.config(text="Question 2")
-            answer4_button.config(text="Question 2")
-        elif Button.counter == 7:
-            question_label.config(text="Question 9")
-            answer1_button.config(text="Question 2")
-            answer2_button.config(text="Question 2")
-            answer3_button.config(text="Question 2")
-            answer4_button.config(text="Question 2")
-        else:
-            question_label.config(text="Question 10")
-            answer1_button.config(text="Question 2")
-            answer2_button.config(text="Question 2")
-            answer3_button.config(text="Question 2")
-            answer4_button.config(text="Question 2")
-        if Button.counter != 1000:
-            Button.counter += 1
-        else:
-            Button.counter = 0
 
-    def point_system():
-        point = 0
-        Button.counter = 0
-        if Button.counter in range 0
-        point_label.config(text="Point:{}". format(point))
+        anime_prompt = [
+            ["A journey of a certain rubber boy.", "b"],
+         ["A scientist in a new stone world.", "a"],
+        ["Teen eats a cursed item and turns into a king.", "c"],
+        ["Family bound by the gravity of fate", "d"],
+        ["Lovestruck man proves himself through basketball", "a"],
+        ["Humanity in the brink of destruction by the gods"],
+        ["Man-eating giants terrorize a city"],
+        ["Man reincarnation turns him into a monster demon lord"],
+        ["Demon king reincarnation lead him to becoming a student"],
+        []]
+
+
 
     button = IntVar()
     answer1_button = ttk.Radiobutton(root, text="A. MHA", variable=button, value=1)
@@ -98,7 +48,7 @@ def quiz():
     answer3_button.grid(row=1, column=1, padx=5, pady=5)
     answer4_button = ttk.Radiobutton(root, text="D. God of High school", variable=button, value=4)
     answer4_button.grid(row=2, column=1, padx=5, pady=5)
-    confirm_button = ttk.Button(root, text="Confirm", command=lambda: [(change_text(), point_system())])
+    confirm_button = ttk.Button(root, text="Confirm", command=change_text)
     Button.counter = 0
     confirm_button.grid(row=3, column=0, padx=5, pady=5)
 
