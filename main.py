a0229901532123fc3527bb3d007a9048035f0359
Rc19002 <111259906+Rc19002@users.noreply.github.com>
2023-08-27 16:49:17 +1200

    Update main.py
    I removed the Power ups and finished putting the options in for the questions

--- main.py
+++ main.py
@@ -19,16 +19,16 @@ def quiz():
     menu_frame.destroy()  # destroy Menu
 
     anime_prompt = [
-        ["A journey of a certain rubber boy.", "a. test", "b", "c", "d"],
-        ["A scientist in a new stone world.", "a. test", "b", "c", "d"],
-        ["Teen eats a cursed item and turns into a king.", "a. test", "b", "c", "d"],
-        ["Family bound by the gravity of fate", "a. test", "b", "c", "d"],
-        ["Lovestruck man proves himself through basketball", "a. test", "b", "c", "d"],
-        ["Humanity in the brink of destruction by the gods.", "a. test", "b", "c", "d"],
-        ["Man-eating giants terrorize a city.", "a. test", "b", "c", "d"],
-        ["Man reincarnation turns him into a monster demon lord.", "a. test", "b", "c", "d"],
-        ["Demon king reincarnation lead him to becoming a student.", "a. test", "b", "c", "d"],
-        ["Boy captivated by the art of his senior gets inspired to start art.", "a. test", "b", "c", "d"]
+        ["A journey of a certain rubber boy.", "a. MHA", "b. One Piece", "c. One Punch Man", "d. God Of High School"],
+        ["A scientist in a new stone world.", "a. Dr. Stone", "b. New Game", "c. Dr.Rock", "d. Stone World"],
+        ["Teen eats a cursed item and turns into a king.", "a. Demon Slayer", "b. Dark Karate", "c. Jujutsu Kaisen", "d. Code Geass"],
+        ["Family bound by the gravity of fate", "a. Hunter Hunter", "b. Dragon Ball Z", "c. Inazuma 11", "d. JoJo’s Bizarre Adventure"],
+        ["Lovestruck man proves himself through basketball", "a. Slam Dunk", "b. Kuroko no Basket", "c. Fruit Basket", "d. Dribble"],
+        ["Humanity in the brink of destruction by the gods.", "a. God Of High school", "b. Saint Young Man", "c. Record Of Ragnarok", "d. Overlord"],
+        ["Man-eating giants terrorize a city.", "a. Hell's Paradise", "b. Attack On Titan", "c. Promised Neverland", "d. Giant Killing"],
+        ["Man reincarnation turns him into a monster demon lord.", "a. Mushoku Tensei", "b. That Time I Got Reincarnated as a Slime", "c. Overlord", "d. Goblin Slayer"],
+        ["Demon king reincarnation lead him to becoming a student.", "a. Seven Deadly Sin", "b. Demon Lord Daimao", "c. How Not To Summon A Demon Lord", "d. Misfit Of Demon king Academy"],
+        ["Boy captivated by the art of his senior gets inspired to start art.", "a. Kill La Kill", "b. Honey and Clover", "c. Blue Period", "d. Opus Color"]
     ]
 
 
@@ -37,7 +37,8 @@ def quiz():
         global next_question
         question_label = Label(root, text=anime_prompt[next_question][0])
         question_label.grid(row=0, column=0, padx=5, pady=5)
-        point_label = Label(root, text="Point: 0")
+        point = 0
+        point_label = Label(root, text="Point: {}".format(point))
         point_label.grid(row=3, column=1, padx=5, pady=5)
         button = IntVar()
 
@@ -54,14 +55,6 @@ def quiz():
     confirm_button.grid(row=3, column=0, padx=5, pady=5)
     make_question()
 
-    point2x_button = ttk.Button(root, text="2x point")
-    point2x_button.grid(row=0, column=2, padx=5, pady=5)
-    hint_button = ttk.Button(root, text="Hints")
-    hint_button.grid(row=1, column=2, padx=5, pady=5)
-    riddle_button = ttk.Button(root, text="Riddle")
-    riddle_button.grid(row=2, column=2, padx=5, pady=5)
-    answer_button = ttk.Button(root, text="Skip question")
-    answer_button.grid(row=3, column=2, padx=5, pady=5)
 
 next_question = 0
 root.resizable(False, False)
